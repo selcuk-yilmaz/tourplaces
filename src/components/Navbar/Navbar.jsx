7618cae25315a459dbbe0edaa4f9d22420b3344c
selcuklu <yilmax4@gmail.com>
2022-08-09 14:08:54 +0300

    ahref

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
-import "./Navbar.scss"
+import "./Navbar.scss";
 
-const Navbar =()=>{
-    return(
-        <div className="navbar">
-            <a href="">About Us</a>
-            <a href="">For You</a>
-            <a href="">Services</a>
-            <a href="">Blog</a>
-            <a href="">Vlog</a>
-            <a href="">Contact</a>
-        </div>
-    )
-}
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="https://github.com/selcuk-yilmaz">About Us</a>
+      <a href="linkedin.com/in/yilmaz-selcuk">For You</a>
+      <a href="linkedin.com/in/yilmaz-selcuk">Services</a>
+      <a href="https://fireblog-mile.vercel.app/">Blog</a>
+      <a href="https://github.com/selcuk-yilmaz">Vlog</a>
+      <a href="linkedin.com/in/yilmaz-selcuk">Contact</a>
+    </div>
+  );
+};
 export default Navbar;
